2af7cbc9c3b1f65db95be8a5e261cde602d5494b
Aidan Jones <71403696+clause2555@users.noreply.github.com>
2024-10-14 09:51:11 -0400

    Update idt.h

--- OS/kernel/include/kernel/idt.h
+++ OS/kernel/include/kernel/idt.h
@@ -4,39 +4,19 @@
 
 #include <stdint.h>
 
-// Structure of an IDT entry
-struct IDTEntry {
-    uint16_t base_low;    // Lower 16 bits of handler function address
-    uint16_t selector;    // Kernel segment selector
-    uint8_t  zero;        // Reserved, set to 0
-    uint8_t  flags;       // Type and attributes
-    uint16_t base_high;   // Upper 16 bits of handler function address
-} __attribute__((packed));
+typedef struct {
+    uint16_t    isr_low;      // The lower 16 bits of the ISR's address
+    uint16_t    kernel_cs;    // The GDT segment selector that the CPU will load into CS before calling the ISR
+    uint8_t     reserved;     // Set to zero
+    uint8_t     attributes;   // Type and attributes; see the IDT page
+    uint16_t    isr_high;     // The higher 16 bits of the ISR's address
+} __attribute__((packed)) idt_entry_t;
 
-// Structure of the IDT pointer
-struct IDTPointer {
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed));
+typedef struct {
+    uint16_t    limit;
+    uint32_t    base;
+} __attribute__((packed)) idtr_t;
 
-// Function to initialize the IDT
-extern "C" void init_idt();
-
-// Function to handle an interrupt (defined in assembly)
-extern "C" void idt_load(IDTPointer*);
-
-// Function to register an interrupt handler
-extern "C" void register_interrupt_handler(uint8_t interrupt, void (*handler)());
-
-extern "C" void register_interrupt_handler_with_error_code(uint8_t interrupt, void (*handler)(uint32_t));
-
-extern "C" void interrupt_handler_c_with_error_code(uint32_t interrupt_number, uint32_t error_code);
-//extern IDTEntry idt[256] __attribute((aligned(8)));
-//extern "C" void isr0_handler();
-
-//extern "C" void interrupt_handler_c();
-//
-extern "C" void isr14_handler(uint32_t error_code);
+#define IDT_MAX_DESCRIPTORS 256
 
 #endif // IDT_H
-
